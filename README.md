600a23180ee79048b0cf570c244e36dbbd9f09ea
Nathan Lee <esoodev@gmail.com>
2017-09-19 11:14:42 +0900

    Added authentication. Additional functionalities. Now uses connection string.

--- README.md
+++ README.md
@@ -5,64 +5,110 @@ Bare to the bone promise-based module for working with MongoDB suited for the si
 ##Usage Examples
 
 
+### Connection string
+```
+const connection = {
+    uri: "localhost:27017",
+    username: "nathan",
+    password: "yqz992022"
+}
+```
+
 ### Create a collection.
 ```
-var mongosi = require('mongodb-simple');
-mongosi.createCollection('collection_test', 'mongodb://localhost:27017/test');
+var mongosi = require('mongodb-simple')
+mongosi.createCollection(connection, 'users')
 ```
 
 ### Drop a collection.
 ```
-var mongosi = require('mongodb-simple');
-mongosi.dropCollection('collection_test', 'mongodb://localhost:27017/test');
+var mongosi = require('mongodb-simple')
+mongosi.dropCollection(connection, 'users')
 ```
 
 ### Search documents from a collection.
 ```
-var mongosi = require('mongodb-simple');
+var mongosi = require('mongodb-simple')
 
-mongosi.searchDocuments(
-    { name: "text" },
-    'collection_name',
-    'mongodb://localhost:27017/test'
+mongosi.searchDocuments(connection, 'users', {
+    lastname: 'Lee'
+}).then((result) => {
+    console.log(result)
+}, (error) => {
+    console.log(error)
+})
+```
+
+### Find a document by its ID from a collection.
+```
+var mongosi = require('mongodb-simple')
+
+mongosi.getDocumentById(connection, 'users', "59bf5b06705d282d3072effb"
 ).then((result) => {
     console.log(result)
 }, (error) => {
     console.log(error)
-});
+})
 ```
 
 ### Get all documents from a collection.
 ```
-var mongosi = require('mongodb-simple');
+var mongosi = require('mongodb-simple')
 
-mongosi.getAllDocuments(
-    'collection_name',
-    'mongodb://localhost:27017/test'
+mongosi.getAllDocuments(connection, 'users'
 ).then((result) => {
     console.log(result)
 }, (error) => {
     console.log(error)
-});
+})
 ```
 
 ### Insert documents into a collection.
 ```
-var mongosi = require('mongodb-simple');
-var documents = [{name: "Nathan Lee", age: 25}, {name: "Yuqing Zhu", age: 21}];
+var mongosi = require('mongodb-simple')
+var users = [{name: "Nathan Lee", age: 25}, {name: "Yuqing Zhu", age: 21}]
 
-mongosi.insertDocuments(
-    documents,  // array of documents
-    'collection_name',
-    'mongodb://localhost:27017/test'
-).then((result) => {
+mongosi.insertDocuments(connection, 'users', users)
+.then((result) => {
     // returns the inserted documents on success
     console.log(result)
 }, (error) => {
     console.log(error)
-});
+})
+```
+
+### Add a user.
+```
+var mongosi = require('mongodb-simple')
+var roles = 
+{
+    roles: [{
+        role: "root",
+        db: "admin"
+    }]
+}
+
+mongosi.addUser(connection, 'nathan', 'password', roles)
+```
+
+### Add an admin.
+```
+var mongosi = require('mongodb-simple')
+
+mongosi.addAdmin(connection, 'nathan', 'password')
+```
+
+### Remove an user.
+```
+var mongosi = require('mongodb-simple')
+
+mongosi.removeUser(connection, 'nathan')
 ```
 
 
+
 ##Updates
+###1.1.0
+Added authentication. Additional functionalities.
+Now uses connection string.
 
--- mongodb-simple.js
+++ mongodb-simple.js
@@ -1,101 +1,219 @@
 /**
  * Methods pertaining to various mongodb operations.
  */
-
+var ObjectId = require('mongodb').ObjectID
 var MongoClient = require('mongodb').MongoClient
 var assert = require('assert')
 
 module.exports = {
 
-    searchDocuments: function (search, collection_name, db_url) {
+    searchDocuments: function (connection, collection_name, search) {
+
+        var url = this._reformUri(connection.uri, connection.username, connection.password)
+
         return new Promise((resolve, reject) => {
 
-            MongoClient.connect(db_url, (err, db) => {
-                var collection = db.collection(collection_name);
+            MongoClient.connect(url, (err, db) => {
+                var collection = db.collection(collection_name)
 
                 collection.find(search).toArray(function (err, docs) {
-                    if (err) reject(err);
-                    db.close();
-                    resolve(docs);
-                });
-            });
-        });
+                    if (err) {
+                        console.log('Error while searching document ' + search + ' in ' + collection_name + '.\n' + err)
+                        reject(err)
+                    }
+                    db.close()
+                    resolve(docs)
+                })
+            })
+        })
     },
 
-    getAllDocuments: function (collection_name, db_url) {
+    getDocumentById: async function (connection, collection_name, id) {
+        var result = await this.searchDocuments(connection, collection_name, {
+            _id: ObjectId(id)
+        })
+
+        return result[0]
+    },
+
+
+    getAllDocuments: function (connection, collection_name) {
+
+        var url = this._reformUri(connection.uri, connection.username, connection.password)
+
         return new Promise((resolve, reject) => {
 
-            MongoClient.connect(db_url, (err, db) => {
-                var collection = db.collection(collection_name);
+            MongoClient.connect(url, (err, db) => {
+                var collection = db.collection(collection_name)
 
                 collection.find({}).toArray(function (err, docs) {
-                    if (err) reject(err);
-                    db.close();
-                    resolve(docs);
-                });
-            });
-        });
+                    if (err) {
+                        console.log(err)
+                        reject(err)
+                    }
+                    db.close()
+                    resolve(docs)
+                })
+            })
+        })
     },
 
-    insertDocuments: function (documents, collection_name, db_url) {
+    insertDocuments: function (connection, collection_name, documents) {
+
+        var url = this._reformUri(connection.uri, connection.username, connection.password)
+
         return new Promise((resolve, reject) => {
 
-            MongoClient.connect(db_url, (err, db) => {
-                var collection = db.collection(collection_name);
+            MongoClient.connect(url, (err, db) => {
+                var collection = db.collection(collection_name)
 
                 collection.insertMany(documents, function (err, result) {
-                    if (err) reject(err);
+                    if (err) {
+                        console.log(err)
+                        reject(err)
+                    }
 
-                    assert.equal(documents.length, result.result.n);
-                    assert.equal(documents.length, result.ops.length);
+                    assert.equal(documents.length, result.result.n)
+                    assert.equal(documents.length, result.ops.length)
 
-                    console.log('Inserted ' + documents.length + ' documents into \"' + collection_name + '\" document collection');
-                    db.close();
-                    resolve(documents);
-                });
-            });
-        });
+                    console.log('Inserted ' + documents.length + ' documents into \"' + collection_name + '\" document collection')
+                    db.close()
+                    resolve(documents)
+                })
+            })
+        })
     },
 
-    createCollection: function (name, db_url) {
+    createCollection: function (connection, collection_name) {
+
+        var url = this._reformUri(connection.uri, connection.username, connection.password)
 
         return new Promise((resolve, reject) => {
 
-            MongoClient.connect(db_url, (err, db) => {
-                db.createCollection(name, {}, function (err, result) {
-                    if (err) reject(err);
+            MongoClient.connect(url, (err, db) => {
+                db.createCollection(collection_name, {}, function (err, result) {
+                    if (err) {
+                        console.log('Error while creating collection ' + collection_name + '.\n' + err)
+                        reject(err)
+                    }
 
                     db.listCollections({
-                        name: name
+                        name: collection_name
                     }).toArray(function (err, names) {
-                        assert.equal(1, names.length);
-                        db.close();
-                        resolve();
-                    });
-                });
-            });
-        });
+                        assert.equal(1, names.length, 'Failed creating collection ' + collection_name)
+                        db.close()
+                        resolve()
+                    })
+                })
+            })
+        })
 
     },
 
-    dropCollection: function (name, db_url) {
+    dropCollection: function (connection, collection_name) {
+
+        var url = this._reformUri(connection.uri, connection.username, connection.password)
 
         return new Promise((resolve, reject) => {
 
-            MongoClient.connect(db_url, (err, db) => {
-                db.dropCollection(name, {}, function (err, result) {
-                    if (err) reject(err);
+            MongoClient.connect(url, (err, db) => {
+                db.dropCollection(collection_name, {}, function (err, result) {
+                    if (err) {
+                        console.log('Error while dropping collection ' + collection_name + '.\n' + err)
+                        reject(err)
+                    }
 
                     db.listCollections({
-                        name: name
+                        name: collection_name
                     }).toArray(function (err, names) {
-                        assert.equal(0, names.length);
-                        db.close();
-                        resolve();
-                    });
-                });
-            });
-        });
+                        assert.equal(0, names.length, 'Failed dropping collection ' + collection_name)
+                        db.close()
+                        resolve()
+                    })
+                })
+            })
+        })
+
+    },
+
+    addUser: function (connection, username, password, roles) {
+
+        var url = this._reformUri(connection.uri, connection.username, connection.password)
+
+        return new Promise((resolve, reject) => {
+
+            MongoClient.connect(url, (err, db) => {
+                if (err) {
+                    console.log('Error while adding user ' + username + '.\n' + err)
+                    reject(err)
+                }
+
+                db.addUser(username, password, {
+                    roles: roles
+                })
+
+                db.close()
+                resolve()
+            })
+        })
+
+    },
+
+    addAdmin: function (connection, username, password) {
+
+        var url = this._reformUri(connection.uri, connection.username, connection.password)
+
+        return new Promise((resolve, reject) => {
+
+            MongoClient.connect(url, (err, db) => {
+                if (err) {
+                    console.log('Error while adding admin.\n' + err)
+                    reject(err)
+                }
+
+                db.addUser(username, password, {
+                    roles: [{
+                        role: "root",
+                        db: "admin"
+                    }]
+                })
+
+                db.close()
+                resolve()
+            })
+        })
+
+    },
+
+    removeUser: function (connection, username) {
+
+        var url = this._reformUri(connection.uri, connection.username, connection.password)
+        console.log(uri)
+
+        return new Promise((resolve, reject) => {
+
+            MongoClient.connect(url, (err, db) => {
+                if (err) {
+                    console.log('Error while removing user ' + username + '.\n' + err)
+                    reject(err)
+                }
+
+                db.removeUser(username)
+
+                db.close()
+                resolve()
+            })
+        })
+
+    },
+
+    _reformUri(uri, username, password) {
+
+        assert(uri, 'Connection URI must be not null.')
+
+        if (username && password)
+            return 'mongodb://' + username + ':' + password + '@' + uri + '/?authMechanism=SCRAM-SHA-1&authSource=admin'
+
+        return 'mongodb://' + uri
     }
-
 }
